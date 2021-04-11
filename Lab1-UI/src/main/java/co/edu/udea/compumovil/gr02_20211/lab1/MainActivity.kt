244e59a6d61bf65cba367518f189bfe190ca9a6a
Joan sebastian gomez acevedo <joan.gomez@udea.edu.co>
2021-04-11 13:58:53 -0500

    update

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20211/lab1/MainActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20211/lab1/MainActivity.kt
@@ -1,11 +1,48 @@
 package co.edu.udea.compumovil.gr02_20211.lab1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.DatePicker
+import androidx.appcompat.app.AppCompatActivity
+import co.edu.udea.compumovil.gr02_20211.lab1.databinding.ActivityMainBinding
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),DatePickerDialog.OnDateSetListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListener()
     }
+
+    private fun setListener(){
+        with(binding){
+            date.setOnClickListener{
+                showDatePickerDialog()
+            }
+            btnNext.setOnClickListener {
+
+            }
+
+
+        }
+    }
+
+    private fun showDatePickerDialog() {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        val newFragment = DatePickerDialog(this, this,year,month,day)
+        newFragment.show()
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        val date = "$dayOfMonth/$month/$year"
+        binding.date.setText(date)
+    }
+
+
 }
+
