0614dfd43bafbd105dd72d0b41f709d875d97e67
Joan sebastian gomez acevedo <jsgomez@playvox.com>
2021-08-30 20:14:35 -0500

    update logout

--- Lab2Activities/src/main/java/co/edu/udea/compumovil/gr02_20211/lab2/view/fragment/PreferenceFragment.kt
+++ Lab2Activities/src/main/java/co/edu/udea/compumovil/gr02_20211/lab2/view/fragment/PreferenceFragment.kt
@@ -6,6 +6,8 @@ import androidx.navigation.fragment.findNavController
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import co.edu.udea.compumovil.gr02_20211.lab2.R
+import co.edu.udea.compumovil.gr02_20211.lab2.data.preferences.EnvironmentPreferences
+import co.edu.udea.compumovil.gr02_20211.lab2.data.preferences.Preferences
 
 class PreferenceFragment : PreferenceFragmentCompat() {
 
@@ -16,6 +18,8 @@ class PreferenceFragment : PreferenceFragmentCompat() {
     override fun onPreferenceTreeClick(preference: Preference?): Boolean {
         preference?.let {
             if (it.key == "logOut") {
+                context?.let { EnvironmentPreferences(it).setPreference(Preferences.USER_LOGGED_IN, false)}
+
                 findNavController().navigate(R.id.action_preferenceFragment_to_loginActivity)
                 activity?.finish()
             }
